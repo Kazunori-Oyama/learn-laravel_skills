a8e8e8d4ba34bfc5f216a20d9af74fcb0967c1ae
Kazunori-Oyama <kazunori0326.t@gmail.com>
2022-05-20 09:31:17 +0900

    add new columns

---
+++ database/migrations/2022_05_20_090912_add_skills_remarks__table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            $table->text('remarks')->comment('特記事項')->after('skill_status')->nullable();
+            $table->integer('experience_years')->comment('経験年数')->after('remarks')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            //
+            $table->dropColumn('remarks');
+            $table->dropColumn('experience_years');
+        });
+    }
+};
